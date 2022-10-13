9b0b3dcffec57c2fd772496851f8e5910b227a79
Vytas77 <vytautas@liv-ex.com>
2022-10-13 10:14:57 +0100

    updated prev button

--- src/Components/Starwars/Starwars.js
+++ src/Components/Starwars/Starwars.js
@@ -13,7 +13,8 @@ const Starwars = (props) => {
         <h3>height: {countContext.data.height}</h3>
         <h3>mass: {countContext.data.mass}</h3>
       </div>
-      <button onClick={() => countContext.countDispatch("prevstarwars")} >Prev</button>
+      {(countContext.countState.starwarsid === 1) ? <button className="grey-button" >Prev</button> : <button onClick={() => countContext.countDispatch("prevstarwars")} >Prev</button> }
+    
       <button onClick={() => countContext.countDispatch("nextstarwars")} >Next</button>
       <button onClick={() => countContext.countDispatch("reset")} >Reset</button>
     </div>
